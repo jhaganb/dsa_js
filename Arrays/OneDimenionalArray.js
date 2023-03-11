b6c3c726620b0e01974df3b893752bfc068df6b5
jhaganb <jhaganb@gmail.com>
2023-03-11 19:08:28 +0530

    added robot bounded in box question - OA

---
+++ Arrays/OneDimenionalArray.js
@@ -0,0 +1,10 @@
+export default class OneDimenionalArray {
+  constructor(sizeOfArray) {
+    this.myArray = new Array(sizeOfArray);
+    this.myArray.fill(0);
+  }
+
+  insert(location,valueToBeInserted){
+    
+  }
+}
