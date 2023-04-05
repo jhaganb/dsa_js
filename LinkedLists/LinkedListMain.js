b302bbc94b7edc79d16d71bb93e3584c94343312
jhaganb <jhaganb@gmail.com>
2023-04-05 13:34:57 +0530

    [added] insertInlinkedList() traverseSinglyLinkedList() searchNode() deletionOfNode() deleteSLL()

--- LinkedLists/LinkedListMain.js
+++ LinkedLists/LinkedListMain.js
@@ -3,5 +3,18 @@ import SinglyLinkedList from "./SinglyLinkedList.js";
 const SLL = new SinglyLinkedList();
 SLL.createSinglyLinkedList(5);
 SLL.insertInlinkedList(6, 0);
-console.log(SLL.head.value);
-console.log(SLL.head.next.value);
+SLL.insertInlinkedList(8, 5);
+SLL.insertInlinkedList(4, 0);
+SLL.insertInlinkedList(7, 1);
+SLL.traverseSinglyLinkedList();
+// SLL.searchNode(6);
+// SLL.searchNode(7);
+// SLL.searchNode(9);
+SLL.deletionOfNode(0)
+SLL.traverseSinglyLinkedList();
+SLL.deletionOfNode(2)
+SLL.traverseSinglyLinkedList();
+SLL.deletionOfNode(8)
+SLL.deleteSLL();
+SLL.traverseSinglyLinkedList();
+
--- LinkedLists/SinglyLinkedList.js
+++ LinkedLists/SinglyLinkedList.js
@@ -31,17 +31,87 @@ class SinglyLinkedList {
       this.tail.next = node;
       this.tail = node;
     } else {
-      const tempNode = new NodeS();
-      let index = 0 ;
-      while(index < location - 1){
+      let tempNode = this.head;
+      let index = 0;
+      while (index < location - 1) {
         tempNode = tempNode.next;
         index++;
       }
-      const nextNode = new NodeS();
+      let nextNode = tempNode.next;
       tempNode.next = node;
       node.next = nextNode;
     }
     this.size++;
+  }
+
+  traverseSinglyLinkedList() {
+    let linkedListString = "";
+    if (this.head === null) {
+      console.log(`SLL does not exist!`);
+    } else {
+      let tempNode = this.head;
+      for (let i = 0; i < this.size; i++) {
+        linkedListString += `${tempNode.value}${
+          i != this.size - 1 ? " -> " : ""
+        }`;
+        tempNode = tempNode.next;
+      }
+    }
+    console.log(linkedListString);
+  }
+
+  searchNode(nodeValue) {
+    if (this.head != null) {
+      let tempNode = this.head;
+      for (let i = 0; i < this.size; i++) {
+        if (tempNode.value === nodeValue) {
+          console.log(`Found the node at location: ${i}`);
+          return true;
+        }
+        tempNode = tempNode.next;
+      }
+    }
+    console.log(`Node not found! `);
+    return false;
+  }
+
+  deletionOfNode(location) {
+    if (this.head === null) {
+      console.log(`The SLL does not exist`);
+      return;
+    } else if (location === 0) {
+      this.head = this.head.next;
+      this.size--;
+      if (this.size === 0) {
+        this.tail = null;
+      }
+    } else if (location >= this.size) {
+      let tempNode = this.head;
+      for (let i = 0; i < this.size - 1; i++) {
+        tempNode = tempNode.next;
+      }
+      if (tempNode === this.head) {
+        this.tail = this.head = null;
+        this.size--;
+        return;
+      }
+      tempNode.next = null;
+      this.tail = tempNode;
+      this.size--;
+    } else {
+      let tempNode = this.head;
+      for (let i = 0; i < location - 1; i++) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = tempNode.next.next;
+      this.size--;
+    }
+  }
+
+  deleteSLL(){
+    this.head = null;
+    this.tail = null;
+    console.log(`The SLL deleted! `);
   }
 }
 
