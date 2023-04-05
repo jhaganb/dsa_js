6a2dbef4a3bc997d87c15e97341f7efb47b7c867
jhaganb <jhaganb@gmail.com>
2023-04-05 12:34:49 +0530

    [created] console.log(SLL.head.value);

--- LinkedLists/LinkedListMain.js
+++ LinkedLists/LinkedListMain.js
@@ -2,4 +2,6 @@ import SinglyLinkedList from "./SinglyLinkedList.js";
 
 const SLL = new SinglyLinkedList();
 SLL.createSinglyLinkedList(5);
+SLL.insertInlinkedList(6, 0);
 console.log(SLL.head.value);
+console.log(SLL.head.next.value);
--- LinkedLists/SinglyLinkedList.js
+++ LinkedLists/SinglyLinkedList.js
@@ -17,6 +17,32 @@ class SinglyLinkedList {
     return this.head;
   }
   //check2
+  insertInlinkedList(nodeValue, location) {
+    const node = new NodeS();
+    node.value = nodeValue;
+    if (this.head === null) {
+      createSinglyLinkedList(nodeValue);
+      return;
+    } else if (location === 0) {
+      node.next = this.head;
+      this.head = node;
+    } else if (location >= this.size) {
+      node.next = null;
+      this.tail.next = node;
+      this.tail = node;
+    } else {
+      const tempNode = new NodeS();
+      let index = 0 ;
+      while(index < location - 1){
+        tempNode = tempNode.next;
+        index++;
+      }
+      const nextNode = new NodeS();
+      tempNode.next = node;
+      node.next = nextNode;
+    }
+    this.size++;
+  }
 }
 
 export default SinglyLinkedList;
